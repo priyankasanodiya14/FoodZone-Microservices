85718c130f5f373c7db8e5714d4dc198b5b884f3
Priyanka Sanodiya <sanodiyapriyanka68@gmail.com>
2023-11-25 23:27:20 +0530

    zomato app

---
+++ Zomato-app/src/main/java/com/demo/ZomatoAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoAppApplication.class, args);
+	}
+
+}
